3c4c03857a10ccc4501f24b556d248e77534cb6b
Sepahrad Salour <sepahrad.salour@gmail.com>
2026-02-11 21:21:45 +0000

    Adding mount settings.

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def run():
 
 
     if pid == 0:
-        os.execv("/bin/bash", ["/bin/bash"])
+        os.system("mount -t proc proc /mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot/proc")
+        os.chroot("/mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot")
+        os.chdir("/")
+        os.execv("/bin/busybox", ["/bin/busybox", "sh"])
     else:
         cid, status = os.waitpid(pid, 0)
-        print(cid, status)
+        os.system("umount /mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot/proc")
+        print(f"Child process {cid} exited with status {status}")
 
 if __name__ == '__main__':
     run()
