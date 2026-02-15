cc00b24c501a04e4ff914cb5e131ea923285dc1f
Sepahrad Salour <sepahrad.salour@gmail.com>
2026-02-15 16:53:16 +0000

    feat: Adding mount namespace.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
+import ctypes
+
+
+libc = ctypes.CDLL("libc.so.6", use_errno=True)
+CLONE_NEWNS = 0x00020000
+
+def unsharens(flags):
+    res = libc.unshare(flags)
+    
+    if res != 0:
+        errno = ctypes.get_errno()
+        raise OSError(errno, f"unshare failed: {os.strerror(errno)}")
 
 def run():
     pid = os.fork()
 
-
     if pid == 0:
+        unsharens(CLONE_NEWNS)
         os.system("mount -t proc proc /mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot/proc")
         os.chroot("/mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot")
         os.chdir("/")
