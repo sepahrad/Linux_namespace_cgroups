9c0bac93c9e9bd0e99366129cdb4802d74493ee9
Sepahrad Salour <sepahrad.salour@gmail.com>
2026-03-08 17:59:25 +0000

    Add PID namespace.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import ctypes
 
 libc = ctypes.CDLL("libc.so.6", use_errno=True)
 CLONE_NEWNS = 0x00020000
+CLONE_NEWUTS = 0x04000000
+CLONE_NEWPID = 0x20000000
 
 def unsharens(flags):
     res = libc.unshare(flags)
@@ -12,11 +14,20 @@ def unsharens(flags):
         errno = ctypes.get_errno()
         raise OSError(errno, f"unshare failed: {os.strerror(errno)}")
 
+def set_hostname(name):
+    res = libc.sethostname(name.encode('utf-8'), len(name))
+    
+    if res != 0:
+        errno = ctypes.get_errno()
+        raise OSError(errno, f"sethostname failed: {os.strerror(errno)}")
+
 def run():
+    unsharens(CLONE_NEWPID)
     pid = os.fork()
 
     if pid == 0:
-        unsharens(CLONE_NEWNS)
+        unsharens(CLONE_NEWNS | CLONE_NEWUTS)
+        set_hostname("mycontainer-ss")
         os.system("mount -t proc proc /mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot/proc")
         os.chroot("/mnt/d/MyData/Projects/Linux_namespace_cgroups/myroot")
         os.chdir("/")
